3cfbc6be9bdd130633129136ad855d66c2ba3266
Keivan Mojmali <mojmalik@gmail.com>
2021-02-06 21:46:07 -0800

    working

--- client/pages/inventory.jsx
+++ client/pages/inventory.jsx
@@ -7,7 +7,8 @@ export default class Inventory extends React.Component {
     super(props);
     this.state = {
       categories: null,
-      categoryData: null
+      categoryData: null,
+      displayModal: false,
     };
     this.setTheState = this.setTheState.bind(this);
     this.handleClick = this.handleClick.bind(this);
@@ -16,7 +17,7 @@ export default class Inventory extends React.Component {
     this.setState(object);
   };
   handleClick(object){
-    console.log(object);
+    console.log('HAHAHAHA',object);
 
     /*
     ADD THE MODAL HERE THAT WILL OPEN UP AND CHANGE THE
@@ -28,11 +29,48 @@ export default class Inventory extends React.Component {
     */
 
 
+
+//handleSubmit
   }
+    displayModal(selectedItem){
+      let modalClass = 'col modal display-none';
+      if(this.state.displayModal === true) {
+        modalClass = 'col modal'
+      }
+      return (
+        <div className={modalClass}>
+          <h4>{selectedItem.itemId}</h4>
+          <form action="#">
+            <div className="form-group">
+              <label htmlFor="type">Type</label>
+              <input className='form-control' type="text" name='type' value={selectedItem.type} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input type="text" className='form-control' name='name' value={selectedItem.name}/>
+            </div>
+            <div className="form-group">
+              <label htmlFor="description">description</label>
+              <input type="text" className='form-control' name='description' value={selectedItem.description}  />
+            </div>
+
+
+
+          </form>
+        </div>
+      )
+    }
+
   render(){
-    console.log('THE STATE',this.state)
+
     return (
+
       <div className="container-fluid h-100">
+
+        <div className="row">
+          {this.displayModal()}
+        </div>
+
         <div className="row">
           <QueryCategories
           setTheState={this.setTheState}
