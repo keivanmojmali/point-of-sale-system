5b36a70cf6d580d8cc63bec9e66903aaa40ced2c
Keivan Mojmali <mojmalik@gmail.com>
2021-02-06 22:12:15 -0800

    working on modal

--- client/pages/inventory.jsx
+++ client/pages/inventory.jsx
@@ -9,6 +9,7 @@ export default class Inventory extends React.Component {
       categories: null,
       categoryData: null,
       displayModal: false,
+      editModal: null
     };
     this.setTheState = this.setTheState.bind(this);
     this.handleClick = this.handleClick.bind(this);
@@ -17,60 +18,70 @@ export default class Inventory extends React.Component {
     this.setState(object);
   };
   handleClick(object){
-    console.log('HAHAHAHA',object);
-
-    /*
-    ADD THE MODAL HERE THAT WILL OPEN UP AND CHANGE THE
-    WITH THE INFO OF THE OBJECT THAT WAS SENT IN AS
-    THE VALUES
-    THEN ONCE IT HAS BEEN SENT AND SUBMITTED -
-    IT WILL PATCH IN THE INFORMATION
-    THEN RE-REQUEST THE CATEGORY DATA AND SET THE STATE
-    */
-
-
-
-//handleSubmit
+    console.log(object);
+    this.setState({displayModal: true, editModal: object});
+  };
+  handleSubmit(){
+    fetch('/api/category/updateItem',{
+      method: 'Patch',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state.editModal)
+    })
+    .then(result=>{
+      return result.json()
+    })
+    .then(data=>{
+      this.setState({displayModal:false, editModal: null})
+    })
+    .catch(err=>console.error(err))
   }
-    displayModal(selectedItem){
-      let modalClass = 'col modal display-none';
-      if(this.state.displayModal === true) {
-        modalClass = 'col modal'
+  onChange(event){
+    this.setState({event.target.name: event.target.value});
+  }
+    displayModal(){
+      if(this.state.displayModal === false) {
+        return;
       }
+      console.log('aaaa',this.state.editModal)
       return (
-        <div className={modalClass}>
-          <h4>{selectedItem.itemId}</h4>
+        <div className='col modal'>
+          <h4>{this.state.editModal.itemId}</h4>
           <form action="#">
             <div className="form-group">
               <label htmlFor="type">Type</label>
-              <input className='form-control' type="text" name='type' value={selectedItem.type} />
+              <input className='form-control' type="text" name='type' value={this.state.editModal.type} onChange={this.onChange} />
             </div>
             <div className="form-group">
               <label htmlFor="name">Name</label>
-              <input type="text" className='form-control' name='name' value={selectedItem.name}/>
+              <input type="text" className='form-control' name='name' value={this.state.editModal.name} onChange={this.onChange}/>
             </div>
             <div className="form-group">
               <label htmlFor="description">description</label>
-              <input type="text" className='form-control' name='description' value={selectedItem.description}  />
+              <input type="text" className='form-control' name='description' value={this.state.editModal.description} onChange={this.onChange}  />
             </div>
-
-
-
+            <div className="form-group">
+              <label htmlFor="price">Price</label>
+              <input type="text" className='form-control' name='price' value={this.state.editModal.price} onChange={this.onChange}/>
+            </div>
+            <div className="form-group">
+              <label htmlFor="stock">Stock</label>
+              <input type="text" name='stock' className='form-control' value={this.state.editModal.stock} onChange={this.onChange}/>
+            </div>
+            <div>
+            <button onClick={this.handleSubmit} type='submit' className=' btn btn-primary btn-lg'> Submit Changes</button>
+            </div>
           </form>
         </div>
       )
     }
-
   render(){
-
     return (
-
       <div className="container-fluid h-100">
-
         <div className="row">
           {this.displayModal()}
         </div>
-
         <div className="row">
           <QueryCategories
           setTheState={this.setTheState}
