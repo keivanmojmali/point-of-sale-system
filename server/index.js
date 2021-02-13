5f655f85286959306c9376311cb4bc3671a765e2
keivanmojmali <mojmalik@gmail.com>
2021-02-12 16:20:59 -0800

    all done

--- server/index.js
+++ server/index.js
@@ -7,7 +7,10 @@ const errorMiddleware = require('./error-middleware');
 
 
 const db = new pg.Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 const app = express();
