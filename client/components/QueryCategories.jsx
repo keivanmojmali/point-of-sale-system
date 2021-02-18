55152eb6d7ddd0928c6c0b8d4ac02921bbd2dd82
keivanmojmali <mojmalik@gmail.com>
2021-02-17 16:03:54 -0800

    working

--- client/components/QueryCategories.jsx
+++ client/components/QueryCategories.jsx
@@ -9,52 +9,55 @@ export default class QueryCategories extends React.Component {
     this.getData = this.getData.bind(this);
     this.getCategoryData = this.getCategoryData.bind(this);
     this.renderCategories = this.renderCategories.bind(this);
-  };
-
-  getData(){
-    fetch('/api/category/getAll')
-    .then(result=>{
-      return result.json()
-    })
-    .then(data=>{
-      this.props.setTheState({'categories': data})
-    })
-      .catch(err=>{
-        console.error(err)
-      })
   }
+
+  getData() {
+    fetch('/api/category/getAll')
+      .then(result => {
+        return result.json();
+      })
+      .then(data => {
+        this.props.setTheState({ categories: data });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
   renderCategories() {
-    if(this.props.categories === null){
+    if (this.props.categories === null) {
       return;
     }
-    return this.props.categories.map((category)=>{
+    return this.props.categories.map(category => {
       return (
         <div className="col d-flex justify-content-center" key={category.type}>
-          <button className='btn btn-primary mt-2 mb-2' onClick={this.getCategoryData}>
+          <button className='btn btn-primary mt-2 mb-2 cat-btn-width' onClick={this.getCategoryData}>
             <h5>{category.type}</h5>
           </button>
         </div>
-      )
-    })
-  }
-  getCategoryData(event) {
-    let cat = event.target.textContent;
-    fetch(`api/category/byType/${cat}`)
-      .then(result => {
-        return result.json()
-      })
-      .then(data => {
-        this.props.setTheState({'categoryData':data})
-      })
-      .catch(err => {
-        next(err)
-      })
+      );
+    });
   }
 
-  componentDidMount(){
+  getCategoryData(event) {
+    const cat = event.target.textContent;
+    fetch(`api/category/byType/${cat}`)
+      .then(result => {
+        return result.json();
+      })
+      .then(data => {
+        this.props.setTheState({ categoryData: data });
+      })
+      .catch(err => {
+        next(err);
+      });
+  }
+
+  componentDidMount() {
     this.getData();
   }
-  render(){
+
+  render() {
     return (
       <div className="col h-100 bg-dark text-light">
         <div className="row category-design border-bottom">
@@ -64,6 +67,6 @@ export default class QueryCategories extends React.Component {
           {this.renderCategories()}
         </div>
       </div>
-    )
-}
+    );
+  }
 }
