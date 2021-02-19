c139d7c93981d1111187c6780f50d43149171c29
keivanmojmali <mojmalik@gmail.com>
2021-02-19 12:00:05 -0800

    all done

--- client/pages/cart.jsx
+++ client/pages/cart.jsx
@@ -8,116 +8,122 @@ export default class Cart extends React.Component {
     this.state = {
       total: null,
       currentOrderArray: null,
-      checkout: false,
+      checkout: false
     };
-    this.queryOrder = this.queryOrder.bind(this)
-    this.renderPage = this.renderPage.bind(this)
-    this.handleCheckout = this.handleCheckout.bind(this)
+    this.queryOrder = this.queryOrder.bind(this);
+    this.renderPage = this.renderPage.bind(this);
+    this.handleCheckout = this.handleCheckout.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleRemove = this.handleRemove.bind(this)
+    this.handleRemove = this.handleRemove.bind(this);
   }
-  queryOrder(){
-    let localStorageId = JSON.parse(localStorage.getItem('currentId'));
-    fetch(`/api/currentOrder/${localStorageId}`)
-    .then(result=>{
-      return result.json()
-    })
-    .then(data=>{
-      this.setState({currentOrderArray: data});
-      let reducerMethod = (accumulator, currentValue) => {
-        return accumulator + currentValue.price;
-      }
-      let total = this.state.currentOrderArray.reduce(reducerMethod, 0);
-      if(!Number.isInteger(total) || total < 0) {
-        this.setState({ total: 0 });
-      } else {
-        this.setState({ total });
-      }
-    })
-    .catch(err=>{
-      console.error(err)
-    })
-  }
-  componentDidMount(){
-    this.queryOrder()
-  }
-  handleCheckout() {
-    this.setState({ checkout: true })
-  }
-  handleRemove(orderItemsId){
 
-    let items = JSON.parse(localStorage.getItem('itemsInCart')) - 1;
-    let newNum = JSON.stringify(items)
+  queryOrder() {
+    const localStorageId = JSON.parse(localStorage.getItem('currentId'));
+    fetch(`/api/currentOrder/${localStorageId}`)
+      .then(result => {
+        return result.json();
+      })
+      .then(data => {
+        this.setState({ currentOrderArray: data });
+        const reducerMethod = (accumulator, currentValue) => {
+          return accumulator + currentValue.price;
+        };
+        const total = this.state.currentOrderArray.reduce(reducerMethod, 0);
+        if (!Number.isInteger(total) || total < 0) {
+          this.setState({ total: 0 });
+        } else {
+          this.setState({ total });
+        }
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
+  componentDidMount() {
+    this.queryOrder();
+  }
+
+  handleCheckout() {
+    this.setState({ checkout: true });
+  }
+
+  handleRemove(orderItemsId) {
+
+    const items = JSON.parse(localStorage.getItem('itemsInCart')) - 1;
+    const newNum = JSON.stringify(items);
     localStorage.setItem('itemsInCart', newNum);
-    this.props.setCartAmount(newNum)
-    fetch('/api/deleteItem',{
-      method:'PATCH',
-      headers:{
+    this.props.setCartAmount(newNum);
+    fetch('/api/deleteItem', {
+      method: 'PATCH',
+      headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({'orderItemsId': orderItemsId })
+      body: JSON.stringify({ orderItemsId: orderItemsId })
     })
-    .then(result=>{
-      return result.json()
-    })
-    .then(data=>{
-      this.queryOrder()
-    })
-    .catch(err=>{
-      console.error(err)
-    })
+      .then(result => {
+        return result.json();
+      })
+      .then(data => {
+        this.queryOrder();
+      })
+      .catch(err => {
+        console.error(err);
+      });
   }
+
   handleSubmit(order) {
-    if (this.state.currentOrderArray.length === 0 ) {
+    if (this.state.currentOrderArray.length === 0) {
       return;
     }
-    let sendTo = order;
+    const sendTo = order;
     sendTo.orderId = this.state.currentOrderArray[0].orderId;
     sendTo.total = this.state.total;
     sendTo.orderArray = this.state.currentOrderArray;
-    this.setState({ checkout: false});
-    fetch('/api/customers/orders',{
+    this.setState({ checkout: false });
+    fetch('/api/customers/orders', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(sendTo)
     })
-    .then(result =>{
-      return result.json();
-    })
-    .then(data=>{
+      .then(result => {
+        return result.json();
+      })
+      .then(data => {
 
-    })
-    .catch(err=>{
-      console.error(err)
-    })
-    let items = 0;
-    let newNum = JSON.stringify(items)
+      })
+      .catch(err => {
+        console.error(err);
+      });
+    const items = 0;
+    const newNum = JSON.stringify(items);
     localStorage.setItem('itemsInCart', newNum);
     this.props.setCartAmount(newNum);
   }
-  renderPage(){
-    if(this.state.checkout === false){
+
+  renderPage() {
+    if (this.state.checkout === false) {
       return <ItemizedCart
-      handleRemove={this,this.handleRemove}
+      handleRemove={this, this.handleRemove}
       currentOrderArray={this.state.currentOrderArray}
       handleCheckout={this.handleCheckout}
-      />
+      />;
     } else {
-      return <Payment handleSubmit={this.handleSubmit} />
+      return <Payment handleSubmit={this.handleSubmit} />;
     }
   }
-  checkoutButton(){
-    if(this.state.checkout === false){
+
+  checkoutButton() {
+    if (this.state.checkout === false) {
       return (
         <button className='btn btn-lg btn-primary'
           onClick={this.handleCheckout}>Checkout</button>
-      )
-    } else {
-      return;
+      );
     }
   }
+
   render() {
     return (
       <div className="col d-flex flex-column justify-content-between h-100">
@@ -131,6 +137,6 @@ export default class Cart extends React.Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
